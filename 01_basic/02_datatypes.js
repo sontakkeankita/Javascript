7deadb5610a0ee23faf2934fbc5739cbe82a1d4e
Ankita Sontakke <119672876+sontakkeankita@users.noreply.github.com>
2024-05-22 17:27:57 +0000

    Datatypes commit

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as a newer version
+
+// alert(3+3) : doesnt execute bcoz we are using node js not browser
+
+console.log(3+3);
+console.log("Ankita");
+
+// datatypes
+// number : 2 to the power 53
+// bigint
+// string : "ankita";
+// boolean : true or false
+// null : standalone value , blank
+// undefined : not defined value yet like let student;
+// symbol : unique
+
+// object 
+console.log(typeof "ankita") // prints string
+console.log(typeof null); // it prints object bcoz null is one kind of object
+console.log(typeof undefined); // prints undefined 
