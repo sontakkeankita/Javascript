06295354471d7c6fa4a4fcdf35377674ca8f7c5a
Ankita Sontakke <119672876+sontakkeankita@users.noreply.github.com>
2024-05-22 17:50:00 +0000

    conversion of datatypes

---
+++ 01_basic/03_ConversionOperations.js
@@ -0,0 +1,12 @@
+let score="33"; // when written undefined instead of 33 the gives NaN
+console.log(typeof score); // string
+console.log(typeof (score)); // string
+
+let ValueInNumber = Number(score);
+console.log( typeof ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1
+
+
